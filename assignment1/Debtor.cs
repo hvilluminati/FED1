61639da267fd853863fa94b3b3ec5136cc9e16a0
hvilluminati <Hvillum.magnus@gmail.com>
2022-12-08 11:38:23 +0100

    Initial commit (unfinished)

---
+++ assignment1/Debtor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Debt
+{
+    public class Debtor
+    {
+
+        public Debtor()
+        {
+        }
+
+        public Debtor(string name, int debt)
+        {
+            _name = name;
+            _debt = debt;
+        }
+
+
+
+        public string _name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int _debt
+        {
+            get
+            {
+                return _debt;
+            }
+            set
+            {
+                _debt = value;
+            }
+        }
+    }
+}
